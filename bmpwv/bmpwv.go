bd5991a95fc7d60b49ac03a6b09f4d238b8761d0
David Rook <hotei1352@gmail.com>
2013-05-05 13:00:46 -0400

    better viewer

--- bmpwv/bmpwv.go
+++ bmpwv/bmpwv.go
@@ -68,13 +68,30 @@ func checkName(pathname string, info os.FileInfo, err error) error {
 
 // given fname.bmp return "<a href="fname.bmp">View fname.bmp</a><br>"
 func makeLine(s string) []byte {
-	return []byte(fmt.Sprintf("<a href=\"%s\">View %s</a><br>\n",s,s))
+	//return []byte(fmt.Sprintf("<a href=\"%s\">View %s</a><br>\n",s,s))
+	return []byte(fmt.Sprintf("<a href= \"%s\"><img src=\"%s\" height=200 width=200>%s Original</a><br>\n",s,s,s))
 }
 
 func init() {
 	pathName := serverRoot
 	g_fileNames = make([]string,0,20)
 	myDir = []byte{}
+	myDir = []byte(`<a href="256colorOS2v1.bmp">View 256colorOS2v1.bmp</a><br>
+<a href="bit1bw-rnr.bmp">View bit1bw-rnr.bmp</a><br>
+<a href="bit1color2.bmp">View bit1color2.bmp</a><br>
+<a href="bit24-test.bmp">View bit24-test.bmp</a><br>
+<a href="bit24-teststrip.bmp">View bit24-teststrip.bmp</a><br>
+<a href="bit24uncomp-marbles.bmp">View bit24uncomp-marbles.bmp</a><br>
+<a href="bit24uncomp-rnr.bmp">View bit24uncomp-rnr.bmp</a><br>
+<a href="bit4-test.bmp">View bit4-test.bmp</a><br>
+<a href="bit4comp-test.bmp">View bit4comp-test.bmp</a><br>
+<a href="bit8-gray-rnr.bmp">View bit8-gray-rnr.bmp</a><br>
+<a href="bit8-test.bmp">View bit8-test.bmp</a><br>
+<a href="bit8.bmp">View bit8.bmp</a><br>
+<a href="bit8comp-rnr.bmp">View bit8comp-rnr.bmp</a><br>
+<a href="bit8comp-test.bmp">View bit8comp-test.bmp</a><br>
+<a href="notBMP.bmp">View notBMP.bmp</a><br>`)
+
 	stats, err := os.Stat(pathName)
 	if err != nil {
 		fmt.Printf("Can't get fileinfo for %s\n", pathName)
@@ -92,33 +109,19 @@ func init() {
 		line := makeLine(val)
 		myDir = append(myDir,line...)
 	}
+	t := []byte("<img src=\"bit8.bmp\">bit8.bmp<br>\n")
+	myDir = append(myDir, t...)
 }
 
-// working but ugly
-func bmpHandler(w http.ResponseWriter, r *http.Request, imageName string) {
-	fmt.Printf("bmpHandler imageName = %s\n",imageName)
-	bf,err := os.Open(imageName)
-	if err != nil {
-		fmt.Printf("bmpHandler cant open bmp %s\n",imageName)
-		return
-	}
-	img,err := bmp.Decode(bf)
-	f,err := os.Create("/www/bmp/png.png")
-	if err != nil {
-		fmt.Printf("%v \n",err)
-		return
-	}
-	wo := bufio.NewWriter(f)
-	png.Encode(wo,img)	
-	wo.Flush()
-	var t = []byte("<img src=" + imageName + ">")
-	w.Write(t)
-}		
 
-// working 
+// working and pretty
 func vbmp(w http.ResponseWriter, r *http.Request) {
 	imageName := serverRoot + r.URL.Path[len(vURL):]
 	fmt.Printf("vbmp imageName = %s\n",imageName)
+	if imageName == serverRoot {
+		w.Write(myDir)
+		return
+	}
 	bf,err := os.Open(imageName)
 	if err != nil {
 		fmt.Printf("bmpHandler cant open bmp %s\n",imageName)
@@ -131,8 +134,21 @@ func vbmp(w http.ResponseWriter, r *http.Request) {
 	w.Write(wo.Bytes())
 }
 
-// working
-func html(w http.ResponseWriter, r *http.Request) {
+// ---------------------------------------------------------------------------     M A I N
+
+func main() {
+//	http.HandleFunc(virtualURL, html)
+	http.HandleFunc(vURL, vbmp)
+	http.Handle(serverRoot,http.StripPrefix(serverRoot, http.FileServer(http.Dir(serverRoot))))
+	log.Printf("Web server is ready at %s\n", listenOn)
+	err := http.ListenAndServe(listenOn, nil)
+	if err != nil {
+		log.Printf("bmpwv: error running webserver %v", err)
+	}
+}
+
+// working but not used
+func dead_html(w http.ResponseWriter, r *http.Request) {
 	var output []byte
 	var err error
 	fmt.Fprintf(w, "<!-- %s %v -->", r.Method, r.URL) // debug request input
@@ -157,7 +173,7 @@ func html(w http.ResponseWriter, r *http.Request) {
 		return		
 	}		
 	if ext == ".bmp" {
-		bmpHandler(w,r,fileName)
+		dead_bmpHandler(w,r,fileName)
 		return
 	} else {
 		output, err = ioutil.ReadFile(fileName)
@@ -170,15 +186,24 @@ func html(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "<-- %q %q-->",fileName, ext)
 }
 
-
-func main() {
-//	http.HandleFunc(virtualURL, html)
-	http.HandleFunc(vURL, vbmp)
-	http.Handle(serverRoot,http.StripPrefix(serverRoot, http.FileServer(http.Dir(serverRoot))))
-	log.Printf("Web server is ready at %s\n", listenOn)
-	err := http.ListenAndServe(listenOn, nil)
+// working but ugly
+func dead_bmpHandler(w http.ResponseWriter, r *http.Request, imageName string) {
+	fmt.Printf("bmpHandler imageName = %s\n",imageName)
+	bf,err := os.Open(imageName)
 	if err != nil {
-		log.Printf("bmpwv: error running webserver %v", err)
+		fmt.Printf("bmpHandler cant open bmp %s\n",imageName)
+		return
 	}
-}
+	img,err := bmp.Decode(bf)
+	f,err := os.Create("/www/bmp/png.png")
+	if err != nil {
+		fmt.Printf("%v \n",err)
+		return
+	}
+	wo := bufio.NewWriter(f)
+	png.Encode(wo,img)	
+	wo.Flush()
+	var t = []byte("<img src=" + imageName + ">")
+	w.Write(t)
+}		
 
