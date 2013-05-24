fbb68fc306d9c923fdb469ecd73c77e35545bbea
David Rook <hotei1352@gmail.com>
2013-05-24 16:44:34 -0400

    bmpwv serves all type of images from image directory

--- bmpwv/bmpwv.go
+++ bmpwv/bmpwv.go
@@ -10,7 +10,8 @@ import (
 	//"bufio"
 	"bytes"
 	"fmt"
-	"image"
+	//"image"
+	"image/jpeg"
 	"image/png"
 	//"io"
 	"io/ioutil"
@@ -18,16 +19,19 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-
+	"strings"
 //"time"
 )
 
 const (
 	hostIPstr  = "127.0.0.1"
 	portNum    = 8282
+	
 	bmpURL = "/bmp/"
-	mdURL  = "/md/"
 	imageURL = "/image/"
+	mdURL  = "/md/"
+	pngURL = "/png/"
+	
 	serverRoot = "/www/"
 )
 
@@ -40,6 +44,7 @@ var (
 var myBmpDir = []byte{}
 var myMdDir = []byte{}
 var myImageDir = []byte{}
+var myPngDir = []byte{}
 
 func checkBmpName(pathname string, info os.FileInfo, err error) error {
 	//fmt.Printf("checking %s\n", pathname)
@@ -185,58 +190,108 @@ func htmlFromMd(fname string) []byte {
 	return output
 }
 
-// testing now
+// tested ok with bmp,png,jpeg,jpg
 func imageHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == imageURL {
 		w.Write(myImageDir)
 		return
 	}
 	workDir := serverRoot + imageURL[1:]
-	fmt.Printf("workDir(%s)\n",workDir)
-	fmt.Printf("r.URL.Path(%s)\n",r.URL.Path)
+	//fmt.Printf("workDir(%s)\n",workDir)
+	//fmt.Printf("r.URL.Path(%s)\n",r.URL.Path)
 	imageName := workDir+r.URL.Path[len(imageURL):]
 	fmt.Printf("imageHandler: imageName = %s\n", imageName)
-	bf, err := os.Open(imageName)
-	if err != nil {
-		fmt.Printf("imageHandler: cant open image %s\n", imageName)
+	ext := strings.ToLower(filepath.Ext(imageName))
+		//fmt.Printf("ext = %s\n",ext)
+	if ext == ".bmp" {		
+		bmpWriteOut(imageName,w)
 		return
 	}
-	img, s, err := image.Decode(bf)
+	if ext == ".png" {		
+		pngWriteOut(imageName,w)
+		return
+	}
+	if ext == ".jpeg" {		
+		jpegWriteOut(imageName,w)
+		return
+	}
+	if ext == ".jpg" {		
+		jpegWriteOut(imageName,w)
+		return
+	}
+	if ext == ".svg" {		
+		rawWriteOut(imageName,w)
+		return
+	}	
+}
+
+// just hand the raw file over to the browser
+func rawWriteOut(fileName string, w http.ResponseWriter) {	
+	rawBuf, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		fmt.Printf("imageHandler: image decode failed for %s with type: %s\n", imageName,s)
-		w.Write([]byte(fmt.Sprintf("image Decode failed for %s with type: %s\n",imageName,s)))
+		fmt.Printf("rawWriteOut: cant open file %s\n", fileName)
+		return
+	}
+	w.Write(rawBuf)
+}
+
+func jpegWriteOut(imageName string, w http.ResponseWriter) {	
+	bf, err := os.Open(imageName)
+	if err != nil {
+		fmt.Printf("jpegWriteOut: cant open image %s\n", imageName)
+		return
+	}
+	img, err := jpeg.Decode(bf)
+		if err != nil {
+			w.Write([]byte(fmt.Sprintf("jpegWriteOut: Decode failed for %s of error:%v\n",imageName,err)))
+			return	
+		}
+		b := make([]byte, 0, 1000*1000)  // b will expand as needed
+		wo := bytes.NewBuffer(b)
+		err = png.Encode(wo, img)
+		if err != nil {
+			fmt.Printf("imageHandler: png encode failed for %s\n", imageName)
+			return	
+		}
+		w.Write(wo.Bytes())		
+}
+
+
+
+// testing
+func pngWriteOut(imageName string, w http.ResponseWriter) {
+	fmt.Printf("pngWriteOut: imageName = %s\n", imageName)
+	bf, err := os.Open(imageName)
+	if err != nil {
+		fmt.Printf("pngWriteOut: cant open image %s\n", imageName)
+		return
+	}
+	img, err := png.Decode(bf)
+	if err != nil {
+		fmt.Printf("pngWriteOut: image decode failed for %s png\n", imageName)
+		w.Write([]byte(fmt.Sprintf("image Decode failed for %s png error:%v\n",imageName,err)))
 		return	
 	}
 	b := make([]byte, 0, 1000*1000)  // b will expand as needed
 	wo := bytes.NewBuffer(b)
 	err = png.Encode(wo, img)
 	if err != nil {
-		fmt.Printf("imageHandler: png encode failed for %s\n", imageName)
+		fmt.Printf("pngWriteOut: png encode failed for %s\n", imageName)
 		return	
 	}
 	w.Write(wo.Bytes())
 }
 
-
-// working and pretty
-func bmpHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == bmpURL {
-		w.Write(myBmpDir)
-		return
-	}
-	workDir := serverRoot + bmpURL[1:]
-	fmt.Printf("workDir(%s)\n",workDir)
-	fmt.Printf("r.URL.Path(%s)\n",r.URL.Path)
-	imageName := workDir+r.URL.Path[len(bmpURL):]
-	fmt.Printf("bmpHandler: imageName = %s\n", imageName)
+func bmpWriteOut(imageName string, w http.ResponseWriter) {
+	fmt.Printf("bmpWriteOut: imageName = %s\n", imageName)
 	bf, err := os.Open(imageName)
 	if err != nil {
-		fmt.Printf("bmpHandler: cant open bmp %s\n", imageName)
+		fmt.Printf("bmpWriteOut: cant open bmp %s\n", imageName)
 		return
 	}
 	img, err := bmp.Decode(bf)
 	if err != nil {
-		fmt.Printf("bmpHandler: bmp decode failed for %s\n", imageName)
+		fmt.Printf("bmpWriteOut: bmp decode failed for %s\n", imageName)
 		w.Write([]byte(fmt.Sprintf("Decode failed for %s\n",imageName)))
 		return	
 	}
@@ -244,7 +299,7 @@ func bmpHandler(w http.ResponseWriter, r *http.Request) {
 	wo := bytes.NewBuffer(b)
 	err = png.Encode(wo, img)
 	if err != nil {
-		fmt.Printf("bmpHandler: png encode failed for %s\n", imageName)
+		fmt.Printf("bmpWriteOut: png encode failed for %s\n", imageName)
 		return	
 	}
 	w.Write(wo.Bytes())
@@ -256,7 +311,6 @@ func bmpHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	//	http.HandleFunc(virtualURL, html)
 	http.HandleFunc(mdURL, mdHandler)
-	http.HandleFunc(bmpURL, bmpHandler)
 	http.HandleFunc(imageURL, imageHandler)
 	
 	// Handle(serverRoot, is like a dir missing an index "ftp-style"
